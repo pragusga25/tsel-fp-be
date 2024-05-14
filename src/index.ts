99a7f280765b65b30447795871fdab8026f0271a
Taufik Pragusga <taufik@pragusga.com>
2024-05-14 21:13:19 +0700

    prepare deploy

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 c();
 
 const app: Express = express();
-const port = process.env.PORT || 8080;
+const port = process.env.$PORT || 8080;
 
 app.use(
   cors({
