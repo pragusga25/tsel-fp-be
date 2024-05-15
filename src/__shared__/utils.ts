33528d902b3ac39140503ea2a8ec8aed2c6ec02a
Taufik Pragusga <taufik@pragusga.com>
2024-05-15 12:54:02 +0700

    domain cookie

--- src/__shared__/utils.ts
+++ src/__shared__/utils.ts
@@ -54,7 +54,7 @@ export class HttpUtil {
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       path: '/',
-      domain: 'tsel.pragusga.com',
+      domain: 'pragusga.com',
     });
   }
 }
