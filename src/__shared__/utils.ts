268f337951491773b20ec31a25372b797644fa16
Taufik Pragusga <taufik@pragusga.com>
2024-05-15 13:14:41 +0700

    domain cookie

--- src/__shared__/utils.ts
+++ src/__shared__/utils.ts
@@ -50,11 +50,10 @@ export class HttpUtil {
   static attachRefreshToken(res: Response, refreshToken: string) {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       path: '/',
-      domain: 'pragusga.com',
     });
   }
 }
